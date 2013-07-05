23dd1a985bb61d02949e16ac4f5fe28a6828a2d0
root <root@siddharth.(none)>
2013-07-05 17:59:47 +0530

    changes

--- train_srilm.sh
+++ train_srilm.sh
@@ -4,6 +4,10 @@ corpus=$1
 order=$2
 lm_file=$3
 
+# read system configuration 
+JOB_SCRIPTS_DIR=`dirname $0`
+. "$JOB_SCRIPTS_DIR/moses_env.conf"
+
 $SRILM_CMD  -order $order \
             -interpolate \
             -kndiscount \
