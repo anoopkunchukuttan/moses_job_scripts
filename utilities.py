41eb55ce0d31925873626f99db4677040f44fc0d
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2013-07-14 21:21:45 +0530

    dataset creation utility added

--- utilities.py
+++ utilities.py
@@ -31,6 +31,38 @@ def select_random_parallel_corpus(fname1,fname2,ofname1,ofname2,no_of_instances)
                        ofile1.write(s1)
                        ofile2.write(s2)
 
+def create_parallel_data_split(fname1,fname2,l1,l2,ofdir,train_size,test_size,tun_size):
+    os.mkdir(ofdir)
+
+    selected_sentences=[]
+    with codecs.open(fname1,'r','utf-8') as file1 :
+        with codecs.open(fname2,'r','utf-8') as file2 :
+           selected_sentences=list(itertools.izip(iter(file1),iter(file2)))
+           random.shuffle(selected_sentences)
+
+    assert(len(selected_sentences)>=train_size+test_size+tun_size)
+
+    # write training set 
+    with codecs.open(ofdir+'/train.'+l1,'w','utf-8') as f1 :
+        with codecs.open(ofdir+'/train.'+l2,'w','utf-8') as f2 :
+            for s1,s2 in selected_sentences[:train_size]:
+                f1.write(s1)
+                f2.write(s2)
+
+    # write test set 
+    with codecs.open(ofdir+'/test.'+l1,'w','utf-8') as f1 :
+        with codecs.open(ofdir+'/test.'+l2,'w','utf-8') as f2 :
+            for s1,s2 in selected_sentences[train_size:train_size+test_size]:
+                f1.write(s1)
+                f2.write(s2)
+
+    # write tuning set 
+    with codecs.open(ofdir+'/tun.'+l1,'w','utf-8') as f1 :
+        with codecs.open(ofdir+'/tun.'+l2,'w','utf-8') as f2 :
+            for s1,s2 in selected_sentences[-tun_size:]:
+                f1.write(s1)
+                f2.write(s2)
 
 if __name__=='__main__':
-    select_random_parallel_corpus(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],int(sys.argv[5]))
+    #select_random_parallel_corpus(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],int(sys.argv[5]))
+    create_parallel_data_split(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],int(sys.argv[6]),int(sys.argv[7]),int(sys.argv[8]))
