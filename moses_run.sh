9cc78183819f1268f2dfb4c1f713bfad377796d2
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2016-03-29 11:11:38 +0530

    avoid truncating the sentences, add timing feature

--- moses_run.sh
+++ moses_run.sh
@@ -57,9 +57,9 @@ then
 
     echo "Cleaning (and lowercase the english only)"
     
-    $SCRIPTS_ROOTDIR/training/clean-corpus-n.perl "$parallel_corpus/train" "$SRC_LANG" "$TGT_LANG" "$WORKSPACE_DIR/cleaned/train.clean" 1 50
-    #cp $parallel_corpus/train.$SRC_LANG $WORKSPACE_DIR/cleaned/train.clean.$SRC_LANG
-    #cp $parallel_corpus/train.$TGT_LANG $WORKSPACE_DIR/cleaned/train.clean.$TGT_LANG
+    #$SCRIPTS_ROOTDIR/training/clean-corpus-n.perl "$parallel_corpus/train" "$SRC_LANG" "$TGT_LANG" "$WORKSPACE_DIR/cleaned/train.clean" 1 50
+    cp $parallel_corpus/train.$SRC_LANG $WORKSPACE_DIR/cleaned/train.clean.$SRC_LANG
+    cp $parallel_corpus/train.$TGT_LANG $WORKSPACE_DIR/cleaned/train.clean.$TGT_LANG
     
     echo "Number of sentences in parallel corpus after cleaning" 
     wc -l "$WORKSPACE_DIR/cleaned/train.clean.$SRC_LANG"
@@ -74,7 +74,7 @@ then
     fi    
     
     echo "Training model started at: " `date`
-    $SCRIPTS_ROOTDIR/training/train-model.perl -external-bin-dir "$SMT_SYSTEM_DIR/bin" \
+    time $SCRIPTS_ROOTDIR/training/train-model.perl -external-bin-dir "$SMT_SYSTEM_DIR/bin" \
             -root-dir "$WORKSPACE_DIR/moses_data" \
             -corpus "$WORKSPACE_DIR/cleaned/train.clean" \
             -e "$TGT_LANG" \
@@ -98,18 +98,19 @@ fi
 if [ $run_tune -eq 0 ]
 then 
     echo "Tuning using MERT started at: " `date`
-   
-    #$SCRIPTS_ROOTDIR/training/mert-moses.pl \
-    #        "$parallel_corpus/tun.$SRC_LANG" \
-    #        "$parallel_corpus/tun.$TGT_LANG" \
-    #        "$MOSES_CMD"  \
-    #        "$WORKSPACE_DIR/moses_data/model/moses.ini" \
-    #         --working-dir "$WORKSPACE_DIR/tuning" \
-    #         --rootdir "$SCRIPTS_ROOTDIR" \
-    #        $MERT_OPTS  > \
-    #         "$WORKSPACE_DIR/log/tuning.out" 2> "$WORKSPACE_DIR/log/tuning.err" 
+  
+    ##### Old tuning command 
+    ####$SCRIPTS_ROOTDIR/training/mert-moses.pl \
+    ####        "$parallel_corpus/tun.$SRC_LANG" \
+    ####        "$parallel_corpus/tun.$TGT_LANG" \
+    ####        "$MOSES_CMD"  \
+    ####        "$WORKSPACE_DIR/moses_data/model/moses.ini" \
+    ####         --working-dir "$WORKSPACE_DIR/tuning" \
+    ####         --rootdir "$SCRIPTS_ROOTDIR" \
+    ####        $MERT_OPTS  > \
+    ####         "$WORKSPACE_DIR/log/tuning.out" 2> "$WORKSPACE_DIR/log/tuning.err" 
 
-    cmd=`echo $SCRIPTS_ROOTDIR/training/mert-moses.pl \
+    cmd=`echo time $SCRIPTS_ROOTDIR/training/mert-moses.pl \
             "$parallel_corpus/tun.$SRC_LANG" \
             "$parallel_corpus/tun.$TGT_LANG" \
             "$MOSES_CMD"  \
@@ -123,7 +124,7 @@ then
     
 
     echo "Running decoder on test set using tuned model started at: " `date`
-    $MOSES_CMD -config "$WORKSPACE_DIR/tuning/moses.ini" \
+    time $MOSES_CMD -config "$WORKSPACE_DIR/tuning/moses.ini" \
                -input-file "$parallel_corpus/test.$SRC_LANG" \
 	       -alignment-output-file "$WORKSPACE_DIR/evaluation/test.align.$TGT_LANG" \
 	       -n-best-list "$WORKSPACE_DIR/evaluation/test.nbest.$TGT_LANG" 10 distinct \
